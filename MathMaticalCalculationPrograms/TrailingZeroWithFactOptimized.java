970df185a3256f367d7f2340dd8d4e8ab07319f9
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-12 14:36:01 +0530

    Create TrailingZeroWithFactOptimized.java

---
+++ MathMaticalCalculationPrograms/TrailingZeroWithFactOptimized.java
@@ -0,0 +1,14 @@
+public class TrailingZeroWithFactOptimized {
+    static int countZero(int n){
+        int result=0;
+        for(int i=5;i<=5;i=i*5){
+            result+=n/i;
+
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("Total zero in the fact is : "+countZero(100));
+    }
+    
+}
