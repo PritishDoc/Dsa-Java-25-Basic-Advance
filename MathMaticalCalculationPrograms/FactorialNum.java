f6cb04b3178eb1bc017d19bcc777130b85aa7c15
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-12 05:39:30 +0530

    factorial num

---
+++ MathMaticalCalculationPrograms/FactorialNum.java
@@ -0,0 +1,16 @@
+public class FactorialNum {
+    static int fact(int x){
+        int fact=1;
+        for(int i=1;i<=x;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        int n=4;
+
+        System.out.println("The factorial num is: "+fact(n));
+    }
+}
+//T(n)=O(n);
+//sF(n)=O(n);
--- MathMaticalCalculationPrograms/Palindrome.java
+++ MathMaticalCalculationPrograms/Palindrome.java
@@ -20,3 +20,4 @@ public class Palindrome {
         }
     }
 }
+//t(n)=O(n)
