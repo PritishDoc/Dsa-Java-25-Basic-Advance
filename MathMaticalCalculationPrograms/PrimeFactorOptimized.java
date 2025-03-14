d99059509ccc5cbfb954619f6b02e3e70ab7f09b
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-14 11:41:12 +0530

    Create PrimeFactorOptimized.java

---
+++ MathMaticalCalculationPrograms/PrimeFactorOptimized.java
@@ -0,0 +1,29 @@
+public class PrimeFactorOptimized {
+    static void pf(int n){
+        if(n<=1) return;
+        while(n%2==0){
+            System.out.println(2);
+            n=n/2;
+        }
+        while(n%3==0){
+            System.out.println(3);
+            n=n/3;
+        }
+        for(int i=5;i*i<=n;i=i+6){
+            while(n%i==0){
+                System.out.println(i);
+                n=n/i;
+            }
+            while(n%(i+2)==0){
+                System.out.println(i+2);
+                n=n/(i+2);
+            }
+        }
+        if(n>3) System.out.println(n);
+    }
+    public static void main(String[] args) {
+        int n=84;
+        pf(n);
+    }
+    
+}
