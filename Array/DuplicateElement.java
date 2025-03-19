8ebb155b0bd4eb5479b8b5e718844a6ad8261be3
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-19 11:19:38 +0530

    Update DuplicateElement.java

--- Array/DuplicateElement.java
+++ Array/DuplicateElement.java
@@ -1,6 +1,19 @@
 public class DuplicateElement {
-    static void reverse(int arr[]){
-        
+    static int  reverse(int arr[]){
+        int res=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=arr[res-1]){
+                arr[res]=arr[i];
+                res++;
+            }
+        }
+        return res;
     }
-    
+    public static void main(String[] args) {
+        int arr[]={2,3,3,3,3,3};
+        System.out.println(reverse(arr));
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
 }
