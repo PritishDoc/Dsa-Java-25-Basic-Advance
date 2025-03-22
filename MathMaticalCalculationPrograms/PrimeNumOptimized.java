f99ac157267800dc260b33de972954bd94f2d7c0
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-22 11:52:12 +0530

    Update PrimeNumOptimized.java

--- MathMaticalCalculationPrograms/PrimeNumOptimized.java
+++ MathMaticalCalculationPrograms/PrimeNumOptimized.java
@@ -1,25 +1,23 @@
 public class PrimeNumOptimized {
-     
-    static int isPrime(int n){
-        
-        
-        if(n<=1) return 0;
-        if(n<=3) return 1;
-        if(n%2==0 || n%3==0) return 0;
-        for(int i=5;i*i<=n;i=i+6){
-            if(n%i==0 || n%(i+2)==0){
-                return 0;
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
             }
         }
-        return 1;
+        return true;
     }
-  
-       
-    
+
     public static void main(String[] args) {
-        int n=11;
-        System.out.println("The num is prime: "+isPrime(n));
-        
+        System.out.println("Prime numbers between 30 and 60:");
+        for (int i = 10; i <= 60; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
     }
-    
 }
