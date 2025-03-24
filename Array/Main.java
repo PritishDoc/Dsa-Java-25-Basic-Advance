b943fa5976d86ce0ca36aa71211b13b7ed3f820e
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-24 08:06:27 +0530

    optimized 1s

---
+++ Array/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+        k x=new K();
+        
+	}
+}
+
+class p{
+    int k;
+}
+
+class k extends p{
+    
+     k=7;
+   k(){
+    System.out.println(k);
+    k=10;
+   }
+    
+}
