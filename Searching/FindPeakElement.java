2c50bc44fb7c80ebcfd3ed209fae5516b0265b62
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-05 10:42:23 +0530

    Update FindPeakElement.java

--- Searching/FindPeakElement.java
+++ Searching/FindPeakElement.java
@@ -2,23 +2,33 @@ package Searching;
 
 public class FindPeakElement {
 
-static int peak(int arr[]){
-    int n=arr.length;
-    if(n==1)return arr[0];
-    if(arr[0]>arr[1])return arr[0];
-    if(arr[n-1]>arr[n-2]) return arr[n-1];
+    static void findAllPeaks(int arr[]) {
+        int n = arr.length;
 
-    for(int i=0;i<n;i++){
-        if(arr[i]>=arr[i+1] && arr[i]>=arr[i-1]){
-            return arr[i];
+        System.out.print("Peak elements: ");
+
+    
+        if (n == 1 || arr[0] > arr[1]) {
+            System.out.print(arr[0] + " ");
         }
+
+        
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] >= arr[i + 1] && arr[i] >= arr[i - 1]) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+       
+        if (arr[n - 1] > arr[n - 2]) {
+            System.out.print(arr[n - 1] + " ");
+        }
+
+        System.out.println(); 
     }
-    return -1;
-}
 
     public static void main(String[] args) {
-        int arr[]={2,4,53,4,4,5,9};
-        System.out.println(peak(arr));
+        int arr[] = {2, 4, 53, 4, 4, 5, 9};
+        findAllPeaks(arr);
     }
-    
 }
