c8119783370104d2eacdcbe5d388060353289e90
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-07 15:40:47 +0530

    Create TwoPointerSum.java

---
+++ Searching/TwoPointerSum.java
@@ -0,0 +1,26 @@
+public class TwoPointerSum {
+static int[] Target(int arr[],int target){
+    int j=arr.length-1;
+    int i=0;
+
+    while(i<j){
+        for(;i<arr.length-1;j++){
+            if(arr[i]+arr[j]==target){
+                return new int[]{i,j};
+            }
+            else {
+                j--;
+            }
+        }
+    }
+    return new int[]{};
+}
+
+
+
+    public static void main(String[] args) {
+        int arr[]={2,32,4,5,4,3,9};
+        System.out.println(Target(arr, 36));
+    }
+    
+}
