8589d0609e5b9661a7c6a0add0f437b77c3fa0ba
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-06 21:22:44 +0530

    Update ArraySortedOptimized.java

--- Array/ArraySortedOptimized.java
+++ Array/ArraySortedOptimized.java
@@ -4,7 +4,7 @@ public class ArraySortedOptimized {
         for(int i=0;i<n;i++){
            
                 if(arr[i]>arr[i+1]){
-                    return false;
+                   
                 }
             }
         
