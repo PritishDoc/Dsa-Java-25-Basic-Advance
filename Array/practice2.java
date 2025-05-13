914747bde3dffa0db734665b30f8e3e88fcb64f4
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-13 20:35:28 +0530

    Update practice2.java

--- Array/practice2.java
+++ Array/practice2.java
@@ -1,11 +1,12 @@
 public class practice2 {
     static int  fibo(int n){
         if(n<=1) return n;
+        return fibo(n-1)+fibo(n-2);
 
     }
     public static void main(String[] args) {
         int a=0,b=1,n=5;
-        for(int i=2;i<=n;i++){
+        for(int i=0;i<=n;i++){
            System.out.println(fibo(i));
         }
     }
