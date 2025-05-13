dc098a39fcf7b1c72f71aa4d2065d50e0773889f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-05-13 19:55:23 +0530

    Create practice2.java

---
+++ Array/practice2.java
@@ -0,0 +1,18 @@
+public class practice2 {
+
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int c=0;
+        for(int i=1;i<=30;i++){
+            
+            System.out.println(a+" ");
+            //System.out.println(c+" ");
+            c=a+b;
+            a=b;
+            b=c;
+        }
+
+    }
+    
+}
